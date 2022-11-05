509dfc4104836e3d757be4d487afeb3de9334a84
lucky071118 <lucky071118@gmail.com>
2022-11-05 16:29:31 +0800

    feat(express-server): get data from mongodb

--- express_server/app.js
+++ express_server/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import connectDB from './db/conn.js'
 
 const app = express();
 const port = 3030;
 
-app.get('/', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.send("Hello World")
+app.get('/', async (req, res) => {
+    const message = await connectDB()
+    res.set('Access-Control-Allow-Origin', '*')
+    res.send(message)
 })
 
 
